df86aa2a5f08bb820a60bf39442c51e5c9b2356b
Josh <jkmilbourne@gmail.com>
2022-01-21 22:12:58 -0700

    Finished functionallity for api call and use of the React.useEffect to handle side effects

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,28 +1,36 @@
-import React from "react";
-import memesData from "./memesData";
+import React, { useState, useEffect } from "react";
 
 export default function Main() {
-  const [meme, setMeme] = React.useState({
+  const [meme, setMeme] = useState({
     topText: "",
     bottomText: "",
     randomImage: "http://i.imgflip.com/1bij.jpg",
   });
 
-  console.log(meme.topText, meme.bottomText);
+  const [allMemes, setAllMemes] = useState([]);
 
-  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+  // Call api to run at render only and save to state allMemes in json format
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setAllMemes(data.data.memes));
+  }, []);
 
+  // click handler for button
   function handleClick(event) {
     event.preventDefault();
-    let num = Math.floor(Math.random() * memesData.data.memes.length);
+    // get random num between 0 and 99
+    let num = Math.floor(Math.random() * allMemes.length);
+    // set state of meme with random image from api call
     setMeme((prevState) => {
       return {
         ...prevState,
-        randomImage: allMemeImages.data.memes[num].url,
+        randomImage: allMemes[num].url,
       };
     });
   }
 
+  // handle change to update state of text inputs
   function handleChange(event) {
     setMeme((prevState) => {
       const { name, value } = event.target;
@@ -35,27 +43,32 @@ export default function Main() {
 
   return (
     <main className="container">
+      {/* main form */}
       <form className="form">
+        {/* for centering the text boxes with flex */}
         <div className="flex">
+          {/* top text box */}
           <input
             type={"text"}
             name="topText"
             id="topText"
-            // value={meme.topText}
+            value={meme.topText}
             onChange={handleChange}
             className="form--top-text input"
             placeholder="Top Text"
           />
+          {/* bottom text box */}
           <input
             type={"text"}
             name="bottomText"
             id="bottomText"
-            // value={meme.bottomText}
+            value={meme.bottomText}
             onChange={handleChange}
             className="form--bottom-text input"
             placeholder="Bottom Text"
           />
         </div>
+        {/* button to rerender meme img with random image image */}
         <button className="form--btn btn" onClick={handleClick}>
           Get a new meme image 🖼
         </button>
@@ -66,6 +79,7 @@ export default function Main() {
             src={meme.randomImage}
             alt=""
           />
+          {/* text boxes that use state to display the top and bottom meme texts dynamicly */}
           <p className="meme-text top-text">{meme.topText}</p>
           <p className="meme-text bottom-text">{meme.bottomText}</p>
         </div>
